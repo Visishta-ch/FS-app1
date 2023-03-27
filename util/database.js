50f7f4bc986fcc9204b9b9c1b0613b818b10fdb0
Visishta-ch <Visishtachatla@gmail.com>
2023-03-27 22:26:53 +0530

    connecting sql db to server

---
+++ util/database.js
@@ -0,0 +1,10 @@
+const mysql = require('mysql2');
+
+const pool = mysql.createPool({
+    host: 'localhost',
+    user: 'root',
+    database: 'test',
+    password: 'Password_123'
+});
+
+module.exports = pool.promise();
